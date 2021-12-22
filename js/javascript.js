3d91735ebc0fbe358ecffe2f64b3b7d664cf2544
Samir Santos <santossamir@hotmail.com>
2021-12-22 10:47:12 -0300

    Adicionando o javaScript para dinamizar a página.

---
+++ js/javascript.js
@@ -0,0 +1,43 @@
+let peopleRaw = localStorage.getItem('people');
+
+	if(peopleRaw != null){
+		var people = JSON.parse(peopleRaw)
+	} else {
+		var people = [];
+	};
+
+function desenhaTabela() {	
+
+    currentLines = [...document.querySelectorAll('table.lista tbody .dinamic-content')];
+    currentLines.forEach((element) => {
+        element.remove()
+     });
+
+	for(person in people){
+		
+	    document.querySelector('table.lista tbody').innerHTML += `
+	    <tr class="dinamic-content" style="background-color: ${((person % 2 == 0) ? '#fff' : '#eee') }">
+	        <td>
+	        	${people[person].name}
+	        </td>
+	        <td>
+	           ${people[person].tel}
+	        </td>
+	        <td>
+	        	${(people[person].xp ? '<strong style="color:green">Sim</strong>' : '<strong style="color:red">Não</strong>')}
+	        </td>
+	        <td>
+	        	<button onclick="deleteUser(${person})">Excluir</button>
+	        	<a href="./form.html?person=${person}">Editar</a>
+	        </td>
+	    </tr>`
+	}
+}
+
+function deleteUser(p){
+	people.splice(p, 1);
+	desenhaTabela();
+	localStorage.setItem('people', JSON.stringify(people))
+};
+
+desenhaTabela();
